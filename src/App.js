12a0f2b61df18ac6ae108b370dfe70759de2b08e
rixiobarrios <rixiobarrios.consulado@gmail.com>
2021-06-15 07:53:37 -0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+// import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Cards from './Cards';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const themeLight = createMuiTheme({
+    palette: {
+        background: {
+            default: '#D3D3D3',
+        },
+    },
+});
+
+const themeDark = createMuiTheme({
+    palette: {
+        background: {
+            default: '#002984',
+        },
+        text: {
+            primary: '#FFFFFF',
+        },
+    },
+});
+
+const App = () => {
+    const [light, setLight] = React.useState(true);
+    return (
+        <MuiThemeProvider theme={light ? themeLight : themeDark}>
+            <CssBaseline />
+            <Button
+                onClick={() => setLight((prev) => !prev)}
+                variant="outlined"
+            >
+                TOGGLE
+            </Button>
+            <Cards />
+        </MuiThemeProvider>
+    );
+};
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
 
 export default App;
---
+++ src/Cards.js
@@ -0,0 +1,135 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+
+import kpr100 from './001_UPDATE-01.png';
+
+const useStyles = makeStyles({
+    root: {
+        maxWidth: 345,
+    },
+    media: {
+        height: '350px',
+    },
+    button: {
+        marginTop: '20px',
+    },
+});
+
+export default function ImgMediaCard() {
+    const classes = useStyles();
+
+    return (
+        <Grid
+            container
+            direction="row"
+            justify="space-evenly"
+            alignItems="center"
+        >
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.media}
+                    image={kpr100}
+                    title="Keep It Real 100 Logo"
+                />
+                <CardActionArea style={{ backgroundColor: '#757DE8' }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Corona Virus Emotional Rescue Game
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textPrimary"
+                            component="p"
+                        >
+                            The CPR game has 100 questions. Please ENTER the
+                            game, and begin!
+                        </Typography>
+                        <Typography align="center">
+                            <Button
+                                className={classes.button}
+                                size="large"
+                                variant="contained"
+                                color="primary"
+                            >
+                                PLAY
+                            </Button>
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.media}
+                    image={kpr100}
+                    title="Keep It Real 100 Logo"
+                />
+                <CardActionArea style={{ backgroundColor: '#757DE8' }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            For The Love of Justice
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textPrimary"
+                            component="p"
+                        >
+                            For the Love of Justice is dedicated to George
+                            Floyd, Ahmaud Arbery, Breonna Taylor and Sandra
+                            Bland.
+                        </Typography>
+                        <Typography align="center">
+                            <Button
+                                className={classes.button}
+                                size="large"
+                                variant="contained"
+                                color="primary"
+                            >
+                                PLAY
+                            </Button>
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+            <Card className={classes.root}>
+                <CardMedia
+                    className={classes.media}
+                    image={kpr100}
+                    title="Keep It Real 100 Logo"
+                />
+                <CardActionArea style={{ backgroundColor: '#757DE8' }}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="h2">
+                            Couple's Love Game
+                        </Typography>
+                        <Typography
+                            variant="body2"
+                            color="textPrimary"
+                            component="p"
+                        >
+                            The CPR Couple's Love Game is designed to help
+                            couples navigate and reflect on their unique
+                            journey.
+                        </Typography>
+                        <Typography align="center">
+                            <Button
+                                className={classes.button}
+                                size="large"
+                                variant="contained"
+                                color="primary"
+                            >
+                                PLAY
+                            </Button>
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Grid>
+    );
+}
