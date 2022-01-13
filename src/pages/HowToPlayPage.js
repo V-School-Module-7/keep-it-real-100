a27379b30ad72cfd15533f11d62a16f26b4a8f99
kimi <sarahkmcaleer@gmail.com>
2022-01-13 12:19:10 -0500

    importing makeStyles to add styling to body text to make text bold, center and  fontSize large

--- src/pages/HowToPlayPage.js
+++ src/pages/HowToPlayPage.js
@@ -6,9 +6,19 @@ import Footer from '../components/Footer'
 import Social from '../components/Social'
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import { makeStyles } from "@material-ui/core/styles";
 
+const useStyles = makeStyles((theme) => ({
+    boldBodyText: {
+        fontWeight: "bolder",
+        fontSize: "large",
+        textAlign: "center"
+      }
+   }));
 
 export default function HowToPlay() {
+
+       const classes = useStyles();
 
     return (
         <div>
@@ -23,8 +33,8 @@ export default function HowToPlay() {
 >           <Box sx={{ mt:5,typography: 'h4', fontWeight: 700}}>
            HOW TO PLAY
             </Box>
-            <Box component="span" sx={{ m:5, p: 5, border: "1px solid black", borderRadius:"10px", backgroundColor:"#747EE8" }}>
-            <Typography variant="body1"><p>Game Instructions
+            <Box component="span" sx={{ m:5, p: 5, border: "1px solid black", borderRadius:"10px", backgroundColor:"#747EE8" }} >
+            <Typography variant="body1" className={classes.boldBodyText}><p>Game Instructions
                Choose someone to go first. Whoever chooses the question is the Leader for that round. The Leader reads the question out loud, then each participant will answer the same question, until every participant has answered. Then, the next person is to choose the next question, and so on. You will see a previous and next button – designed so you can go backwards and forwards, to choose the question you wish to reflect on and discuss. If you would like to see all of the questions at once, and choose from the full list, please click on LIST of Questions, and choose your question from there.</p>
 
                 <p>You will see a button for INTERACTIONS. These are enjoyable and fun group activities for all participants to participate in together when you wish to refresh, recharge and take a break from the conversation. When it is your turn to be the Leader, and you wish to switch to an Interaction, click on the list of Interactions to choose the interaction you wish for your group to interact with. Read aloud to all participants, and have fun!
