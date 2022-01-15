807f8de2798f8ce7b4cb61a56aecae6bba489cc5
kimi <sarahkmcaleer@gmail.com>
2022-01-15 13:55:59 -0500

    Imported Avatar into CreatorBio component; Added new image for profile placeholder to images folder; Added new prop for CreatorBio component called profileImg
    Imported Avatar into CreatorBio component
    Why: Using mui Avatar component to display and style profile pics
    
    Added new image for profile placeholder to images folder
    Why: The profile placeholder image is used for creators who do not have a profile image. I think I need to code a conditional here when we do get images
    
    Added new prop for CreatorBio component called profileImg
    Why: Using prop I want the url reference the correct profile image
    Issue: I'm not sure how to format the image url for the src property here. What file path do I use to reference image files in the images folder?

--- src/pages/CreatorsPage.js
+++ src/pages/CreatorsPage.js
@@ -25,6 +25,7 @@ export default function CreatorsPage() {
       </Grid>
 
       <CreatorBio
+        profileImg={"profile-placeholder"}
         personName={"Leslie"}
         proTitle={"Professional Title"}
         bio="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It waspopularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
@@ -32,6 +33,7 @@ export default function CreatorsPage() {
       ></CreatorBio>
 
 <CreatorBio
+        profileImg={"profile-placeholder"}
         personName={"Laney"}
         proTitle={"Professional Title"}
         bio="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It waspopularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
@@ -39,6 +41,7 @@ export default function CreatorsPage() {
       ></CreatorBio>
 
 <CreatorBio
+        profileImg={"profile-placeholder"}
         personName={"Person Name"}
         proTitle={"Professional Title"}
         bio="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It waspopularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of
