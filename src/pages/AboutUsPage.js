6a26bae1be4157dd03f1cd9a0fba95998c707d7d
xxvxxr <49736963+xxvxxr@users.noreply.github.com>
2022-01-14 17:12:33 -0700

    Xavier b about us page (#12)
    * Updated AboutUsPage
    
    * Updated AboutUsPage
    
    this is the layout and card to start

--- src/pages/AboutUsPage.js
+++ src/pages/AboutUsPage.js
@@ -1,20 +1,79 @@
-import { CssBaseline } from '@material-ui/core'
+import {
+	Box,
+	Container,
+	CssBaseline,
+	makeStyles,
+	Card,
+	Paper,
+	Typography,
+	Avatar,
+} from '@material-ui/core'
+import { grey, indigo, pink } from '@material-ui/core/colors'
 import React from 'react'
 import Footer from '../components/Footer'
 import Social from '../components/Social'
 
+const useStyles = makeStyles({
+	container: {
+		height: '100vh',
+		backgroundColor: grey,
+	},
 
+	paper: {
+		height: 'auto',
+		width: '60vw',
+		display: 'grid',
+		gridAutoFlow: 'column',
+		gap: '16px',
+		placeItems: 'center',
+		backgroundColor: indigo[500],
+		color: indigo[50],
+		padding: '10px',
+	},
+
+	box: {
+		display: 'grid',
+		placeItems: 'center',
+        gap: '24px'
+	},
+	avatar: {
+		// fontSize: '80px',
+		height: '60px',
+		width: '60px',
+	},
+	boldBodyText: {
+		fontWeight: 'bolder',
+		fontSize: 'large',
+		textAlign: 'center',
+	},
+})
 
 export default function AboutUsPage() {
-
-
-    return(
-        <div>
-            <CssBaseline />
-
-            <Social />
-
-            <h1>About Us Page</h1>
-        </div>
-    )
+	const classes = useStyles()
+	return (
+		<div>
+			<CssBaseline />
+			<Container
+				maxWidth="xl"
+				disableGutters="true"
+				className={classes.container}
+			>
+				<Social />
+				<Typography variant='h2' style={{ display: 'flex' , justifyContent: 'center', padding: '16px', margin: '16 0 16 0'}}>About Us Page</Typography>
+				<Box className={classes.box}>
+					<Paper elevation24 className={classes.paper} variant="outlined">
+						<div style={{display: 'grid', placeItems: 'center'}}>
+							<Avatar variant="circular" className={classes.avatar} />
+                            <Typography>this is under</Typography>
+						</div>
+						<Typography className={classes.boldBodyText}>
+							Lorem Ipsum is simply dummy text of the printing and typesetting
+							industry. Lorem Ipsum has been the industry's standard dummy text
+							ever since the 1500s, when an unknown printer took a galley of
+						</Typography>
+					</Paper>
+				</Box>
+			</Container>
+		</div>
+	)
 }
