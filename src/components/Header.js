c36c2b2d65f9b1f5ad9964fdd54e8d835baa313a
Emily Parker <emilyarparker@gmail.com>
2022-02-18 14:22:53 -0700

    Adds the 'limited time beta test' section to the top of the homepage

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,8 @@ import Typography from '@material-ui/core/Typography'
 import { makeStyles } from '@material-ui/core/styles'
 import KIRimg from '../images/001_UPDATE-01.png'
 import Social from './Social'
+import Box from '@mui/material/Box'
+import { Link } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -82,28 +84,58 @@ export default function Header() {
 		<div className={classes.root}>
 			<Social />
 			<AppBar className={classes.appBarNav}>
+			{/* <Grid
+				container
+				direction="column"
+				justifyContent="center"
+				alignItems="center"
+			> */}
+					{/* </Grid> */}
 				<Toolbar className={classes.toolbar}>
+				<Box
+					component="span"
+					sx={{
+						mt: '8rem',
+						h: '300px',
+						p: 5,
+						border: '1px solid black',
+						borderRadius: '10px',
+						backgroundColor: '#fff',
+					}}
+					>
+						<Typography
+							color="textPrimary"
+							style={{ paddingTop: 0, fontWeight: 'bold', fontSize: '2rem', textAlign: 'center' }}
+						>
+							WELCOME to our <span style={{ color: "#D2282A" }}>TIME LIMITED FREE Beta Launch! </span>PLEASE let us know what you 
+							like / love, what could be improved upon, what works or doesn't work for you. We need your 
+							feedback, comments and suggestions to make the final version of Keep It Real 100 
+							as impactful and awesome as possible. We are excited to hear from you!
+							<br/>
+						<Link href='mailto:RobinsonL3@aol.com' underline="hover" target="_blank"  style={{ color: 'black', fontWeight: 'bold', fontSize: '1rem', textAlign: 'center' }}>RobinsonL3@aol.com</Link>
+						</Typography>
+					</Box>
 					<img
 						className={classes.logo}
 						src={KIRimg}
 						alt="Keep It Real 100 Logo"
-					/>
+						/>
 					<Typography
 						className={classes.title}
 						style={{
 							fontWeight: 'Bold 700 ',
-							fontSize: '6rem',
+							fontSize: '4rem',
 							lineHeight: '1.43',
 							letterSpacing: '0.01041em',
 						}}
-					>
+						>
 						Keep It Real 100 Game
 					</Typography>
 					<Typography
 						className={classes.title}
 						color="textPrimary"
-						style={{ paddingTop: 0, fontWeight: 'bold', fontSize: '3rem' }}
-					>
+						style={{ paddingTop: 0, fontWeight: 'bold', fontSize: '2rem' }}
+						>
 						for BIPOC Youth, Adults, & Allies
 					</Typography>
 				</Toolbar>
